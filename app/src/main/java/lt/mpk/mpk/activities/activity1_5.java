ce86bafaae7079280433571f9dafd5bb17e85d71
Aruncikas <baronas15@gmail.com>
2016-07-31 08:34:11 +0300

    Fix: No longer able to play more than one item at once

--- app/src/main/java/lt/mpk/mpk/activities/activity1_5.java
+++ app/src/main/java/lt/mpk/mpk/activities/activity1_5.java
@@ -164,10 +164,10 @@ public class activity1_5 extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         super.onBackPressed();
-
-        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+        Exit();
     }
 
     public void activity15button2_Click(View v)
@@ -176,6 +176,14 @@ public class activity1_5 extends AppCompatActivity {
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         intent.putExtra("EXIT", true);
         startActivity(intent);
+        Exit();
+    }
+
+    private void Exit()
+    {
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mHandler.removeCallbacksAndMessages(null);
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
     }
 }
