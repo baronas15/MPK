7b1827d745a02c528260dd45ec77486283652289
Aruncikas <baronas15@gmail.com>
2016-07-31 15:57:12 +0300

    Further cleanup

--- app/src/main/java/lt/mpk/mpk/activities/main.java
+++ app/src/main/java/lt/mpk/mpk/activities/main.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import lt.mpk.mpk.R;
@@ -16,47 +15,41 @@ import lt.mpk.mpk.R;
 public class main extends AppCompatActivity {
     private Typeface tf;
     private Toolbar myToolbar;
+    private int enterAnim = R.anim.slide_in_right;
+    private int exitAnim = R.anim.slide_out_right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Initialize variables */
+        /* Create toolbar */
         myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        tf = Typeface.createFromAsset(getAssets(), "fonts/express.ttf");
-
         setSupportActionBar(myToolbar);
 
         //region ChangeFonts
+        tf = Typeface.createFromAsset(getAssets(), "fonts/goodfish.ttf");
         TextView tv = (TextView) findViewById(R.id.activity_main_title);
-        if (tv != null) {
-            tv.setTypeface(tf);
-        }
+        tv.setTypeface(tf);
+
+        tf = Typeface.createFromAsset(getAssets(), "fonts/express.ttf");
         TextView tv1 = (TextView) findViewById(R.id.activity_main_text1);
-        if (tv1 != null) {
-            tv1.setTypeface(tf);
-        }
+        tv1.setTypeface(tf);
+
         TextView tv2 = (TextView) findViewById(R.id.activity_main_text2);
-        if (tv2 != null) {
-            tv2.setTypeface(tf);
-        }
+        tv2.setTypeface(tf);
+
         TextView tv3 = (TextView) findViewById(R.id.activity_main_text3);
-        if (tv3 != null) {
-            tv3.setTypeface(tf);
-        }
+        tv3.setTypeface(tf);
 
         TextView tv4 = (TextView) findViewById(R.id.activity_main_titleText);
-        if (tv4 != null) {
-            tv4.setTypeface(tf);
-        }
+        tv4.setTypeface(tf);
+
         TextView tv5 = (TextView) findViewById(R.id.activity_main_button1);
-        if (tv5 != null) {
-            tv5.setTypeface(tf);
-        }
+        tv5.setTypeface(tf);
+
         //endregion
     }
-
 
     /*@Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -83,23 +76,22 @@ public class main extends AppCompatActivity {
         return true;
     }
 
-
     //region Redirecting
     public void ButtonClick1(View v){
         startActivity(new Intent(main.this, activity1.class));
-        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
+        overridePendingTransition(enterAnim,exitAnim);
     }
     public void PanelClick1(View v){
         startActivity(new Intent(main.this, activity2.class));
-        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
+        overridePendingTransition(enterAnim,exitAnim);
     }
     public void PanelClick2(View v){
         startActivity(new Intent(main.this, activity3.class));
-        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
+        overridePendingTransition(enterAnim,exitAnim);
     }
     public void PanelClick3(View v){
         startActivity(new Intent(main.this, activity4.class));
-        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
+        overridePendingTransition(enterAnim,exitAnim);
     }
     //endregion
 }
