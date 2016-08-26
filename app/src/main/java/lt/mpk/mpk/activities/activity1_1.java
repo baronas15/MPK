02b12a6526dc12426c0488173073e9eaab22dad5
Aruncikas <baronas15@gmail.com>
2016-08-27 02:51:25 +0300

    -

--- app/src/main/java/lt/mpk/mpk/activities/activity1_1.java
+++ app/src/main/java/lt/mpk/mpk/activities/activity1_1.java
@@ -1,10 +1,16 @@
 package lt.mpk.mpk.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -13,7 +19,8 @@ import lt.mpk.mpk.R;
 import lt.mpk.mpk.app;
 
 public class activity1_1 extends AppCompatActivity {
-
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +66,104 @@ public class activity1_1 extends AppCompatActivity {
             }
         });
         //endregion
+
+        //region Toolbar_NavDrawer
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        ab.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("MPK");
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        FrameLayout f1 = (FrameLayout) findViewById(R.id.drawer_item_1);
+        f1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mpk.lt"));
+                startActivity(browserIntent);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f2 = (FrameLayout) findViewById(R.id.drawer_item_2);
+        f2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), main.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f3 = (FrameLayout) findViewById(R.id.drawer_item_3);
+        f3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity2.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f4 = (FrameLayout) findViewById(R.id.drawer_item_4);
+        f4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity3.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f5 = (FrameLayout) findViewById(R.id.drawer_item_5);
+        f5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity4.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f7 = (FrameLayout) findViewById(R.id.drawer_item_7);
+        f7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.kaunas.lt/"));
+                startActivity(browserIntent);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f8 = (FrameLayout) findViewById(R.id.drawer_item_8);
+        f8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.kaunovsb.lt/"));
+                startActivity(browserIntent);
+                mDrawer.closeDrawers();
+            }
+        });
+        //endregion
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void activity12button1_Click(View v){
@@ -158,28 +263,3 @@ public class activity1_1 extends AppCompatActivity {
     }
     //endregion
 }
-
-
-/*
-
-        SharedPreferences settings = getSharedPreferences("Preferences", 0);
-
-        switch (settings.getInt("mental",3)) {
-            case R.id.mindgroup_1:
-                //Mind was selected 1
-                break;
-            case R.id.mindgroup_2:
-                //Mind was selected 2
-                break;
-            case R.id.mindgroup_3:
-                //Mind was selected 3
-                break;
-            case R.id.mindgroup_4:
-                //Mind was selected 4
-                break;
-            case R.id.mindgroup_5:
-                //Mind was selected 5
-                break;
-        }
-
-*/
