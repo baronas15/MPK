e3c74600a0835c1b4c332a66bcf6fc77c46233b5
Aruncikas <baronas15@gmail.com>
2016-08-27 02:45:45 +0300

    -

--- app/src/main/java/lt/mpk/mpk/activities/activity1.java
+++ app/src/main/java/lt/mpk/mpk/activities/activity1.java
@@ -1,21 +1,17 @@
 package lt.mpk.mpk.activities;
 
-import android.animation.Animator;
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
-import android.animation.ValueAnimator;
 import android.content.Intent;
-import android.provider.ContactsContract;
+import android.net.Uri;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
-import android.view.animation.ScaleAnimation;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,12 +19,15 @@ import lt.mpk.mpk.R;
 import lt.mpk.mpk.app;
 
 public class activity1 extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity1);
 
+        //region Activity1
         app a = ((app)getApplicationContext());
         a.setEmotional1(-1);
         a.setEmotional2(-1);
@@ -66,9 +65,7 @@ public class activity1 extends AppCompatActivity {
             }
 
             @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
+            public void onAnimationRepeat(Animation animation) {}
         });
 
         up.setAnimationListener(new Animation.AnimationListener() {
@@ -85,20 +82,117 @@ public class activity1 extends AppCompatActivity {
             }
 
             @Override
-            public void onAnimationRepeat(Animation animation) {
+            public void onAnimationRepeat(Animation animation) {}
+        });
+        //endregion
 
+        //region Toolbar_NavDrawer
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        ab.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("MPK");
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        FrameLayout f1 = (FrameLayout) findViewById(R.id.drawer_item_1);
+        f1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mpk.lt"));
+                startActivity(browserIntent);
+                mDrawer.closeDrawers();
             }
         });
+        FrameLayout f2 = (FrameLayout) findViewById(R.id.drawer_item_2);
+        f2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), main.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f3 = (FrameLayout) findViewById(R.id.drawer_item_3);
+        f3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity2.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f4 = (FrameLayout) findViewById(R.id.drawer_item_4);
+        f4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity3.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f5 = (FrameLayout) findViewById(R.id.drawer_item_5);
+        f5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), activity4.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f7 = (FrameLayout) findViewById(R.id.drawer_item_7);
+        f7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.kaunas.lt/"));
+                startActivity(browserIntent);
+                mDrawer.closeDrawers();
+            }
+        });
+        FrameLayout f8 = (FrameLayout) findViewById(R.id.drawer_item_8);
+        f8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.kaunovsb.lt/"));
+                startActivity(browserIntent);
+                mDrawer.closeDrawers();
+            }
+        });
+        //endregion
     }
 
-    public void activity1button1_Click(View v){
-        startActivity(new Intent(activity1.this, activity1_1.class));
-        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+    }
+
+    public void activity1button1_Click(View v){
+        startActivity(new Intent(activity1.this, activity1_1.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
     }
 }
